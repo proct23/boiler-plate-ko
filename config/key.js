4be691040886cc6a42a13d6e4146c11b7584a82c
proct23 <proct23@naver.com>
2020-11-15 23:53:14 +0900

    비밀

--- config/key.js
+++ config/key.js
@@ -1,7 +1,5 @@
 if(process.env.NODE_ENV ==='production') {
     module.exports = require('./prod')
-    console.log("dsfsdfdsfsd")
 } else {
     module.exports = require('./dev')
-    console.log("dsfsdfdsfsd 씨발")
 }
